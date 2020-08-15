b71d44b52e959995b7383cacb7f607f6a87f39ea
Ginner Zapata <ginnerzapata@gmail.com>
2020-08-15 01:17:15 -0300

    game starts with player names error dur to default data

--- main.js
+++ main.js
@@ -10,19 +10,20 @@ const DEAFULTBOARD = [
   { class: "board-card", id: "8" },
 ];
 
-const Player = (name, marker) => {
+const Player = (name) => {
   const movements = [];
   let score = 0;
+  marker = name[0].toUpperCase();
   return { name, marker, movements, score };
 };
 
-const player1 = Player("Ginner", "G");
-const player2 = Player("Lucia", "L");
+let player1 = Player("Ginner", "G");
+let player2 = Player("Lucia", "L");
 
 const DOM = (() => {
   const _container = document.getElementById("container");
-  const _board = document.getElementById("board");
-  return { _container, _board };
+  //const _board = document.getElementById("board");
+  return { _container };
 })();
 
 const board = (() => {
@@ -39,15 +40,21 @@ const board = (() => {
   let boardCards = [...DEAFULTBOARD];
   const playedSpots = [];
   const renderBoardCards = () => {
+    const _board = document.createElement("div");
+    _board.id = "board";
     for (card of boardCards) {
       const div = document.createElement("div");
       div.className = card.class;
       div.id = card.id;
-      DOM._board.appendChild(div);
+      _board.appendChild(div);
     }
+    DOM._container.appendChild(_board);
   };
   const reset = () => {
-    DOM._board.innerHTML = "";
+    board.playedSpots.length = 0;
+    player1.movements.length = 0;
+    player2.movements.length = 0;
+    DOM._container.innerHTML = "";
     boardCards = [...DEAFULTBOARD];
   };
 
@@ -65,7 +72,7 @@ const displayGameInfo = (() => {
       <div class="game-info" id="scores">
         <p class="score">${player1.score}</p>
         <p class="current-player">${gameLogic.currentPlayer.name}</p>
-        <p class="score">${player1.score}</p>
+        <p class="score">${player2.score}</p>
       </div>
     `;
     DOM._container.insertAdjacentHTML("afterbegin", displayHTML);
@@ -80,6 +87,10 @@ const gameLogic = (() => {
     currentPlayer === player1
       ? (currentPlayer = player2)
       : (currentPlayer = player1);
+    if (DOM._container.querySelector(".current-player")) {
+      DOM._container.querySelector(".current-player").innerText =
+        currentPlayer.name;
+    }
   };
 
   const checkIfWinner = (arr) => {
@@ -88,9 +99,21 @@ const gameLogic = (() => {
     }
   };
   //dom manupulation and logic. Must be simplified
-  DOM._board.addEventListener("click", (e) => {
+  DOM._container.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const p1 = document.getElementById("player1");
+    const p2 = document.getElementById("player2");
+    player1 = Player(p1.value);
+    player2 = Player(p2.value);
+    gameLogic.newGame();
+  });
+
+  DOM._container.addEventListener("click", (e) => {
     if (e.target.className.includes("gradient")) {
       return;
+    }
+    if (e.target.tagName === "BUTTON") {
+      gameLogic.newGame();
     }
     if (e.target.className.includes("board-card")) {
       let gradient =
@@ -101,13 +124,21 @@ const gameLogic = (() => {
 
       board.playedSpots.push(+e.target.id);
       currentPlayer.movements.push(+e.target.id);
+      if (!checkIfWinner(board.winningCombos) && board.playedSpots.length >= 9)
+        draw();
       if (checkIfWinner(board.winningCombos)) {
-        console.log(`${currentPlayer.name} Wins!`);
         win();
       }
       changeCurrentPlayer();
     }
   });
+
+  const draw = () => {
+    DOM._container.innerHTML = `
+    <img src="img/draw.svg" alt="trophy" />
+    <h1>It's a draw</h1>
+    <button >Play Again</button>`;
+  };
   const win = () => {
     currentPlayer.score++;
     DOM._container.innerHTML = `
@@ -116,8 +147,39 @@ const gameLogic = (() => {
     <button >Play Again</button>`;
   };
 
-  return { changeCurrentPlayer, currentPlayer };
+  const newGame = () => {
+    board.reset();
+    displayGameInfo.renderDisplay();
+    board.renderBoardCards();
+  };
+
+  return { changeCurrentPlayer, currentPlayer, newGame };
 })();
 
-displayGameInfo.renderDisplay();
-board.renderBoardCards();
+const form = (() => {
+  const formHTML = `
+  <form id="form">
+    <input
+      type="text"
+      name="player-name"
+      id="player1"
+      placeholder="Player 1 name"
+      value="Player 1"
+    />
+    <input
+      type="text"
+      name="player-name"
+      id="player2"
+      placeholder="Player 2 name"
+      value="Player 2"
+    />
+    <p>Leave in blank to get default values</p>
+    <input type="submit" value="Start game" />
+</form>`;
+
+  const render = () => {
+    DOM._container.insertAdjacentHTML("afterbegin", formHTML);
+  };
+  return { render };
+})();
+form.render();
